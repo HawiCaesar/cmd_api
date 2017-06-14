d2d24197f70d8b8b885ae703c4c6343377391cd6
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-14 16:14:52 +0300

    Add test case when no data is returned from api, add program logic for app

--- HttpWebTestCase.py
+++ HttpWebTestCase.py
@@ -6,9 +6,12 @@ class HttpWebTestCase(unittest.TestCase):
 	url = 'https://jsonplaceholder.typicode.com/posts'
 
 	# Check for result from API
-	def test_api_data_200OK(self):
+	def test_api_data(self):
 		self.assertIsInstance(get_api_data(self.url, '3'), requests.models.Response, "200 OK from API can is reachable and accessible")
 
+	# Check for no result, 404
+	def test_api_data_no_data_returned(self):
+		self.assertEqual(get_api_data(self.url, 'abc'), "No post macthes your request.Please enter an integer.", "No post is found because of non-integers")
 
 
 
--- http_web.py
+++ http_web.py
@@ -2,15 +2,39 @@ import requests
 
 def get_api_data(url, post_id):
 
-	response = requests.get(url + post_id)
-	return response
+	if post_id.isdigit():
+
+		response = requests.get(url + post_id)
+		return response
+
+	else:
+		try:
+			int(post_id)
+			response = requests.get(url + post_id)
+			return response
+
+		except ValueError as error:
+			return "No post macthes your request.Please enter an integer."
 		
 
 def post_data_to_api(post_url):
 	pass
 
 def main():
-	get_api_data('https://jsonplaceholder.typicode.com/posts/', '3')
+
+	print "******** Command Line API Application ********"
+
+	post_number = raw_input("Please enter a number between 1 and 100 \n")
+
+	print "Fetching your the post you want....\n"
+
+	response = get_api_data('https://jsonplaceholder.typicode.com/posts/', post_number)
+	print("\tGET Response data\n\t%s\n%s\n\tStatus code\n\t%s\n%s\n\tHeaders\n\t%s\n%s" % 
+		("-"*17,response.text, "-"*11,response.status_code,"-"*7, response.headers))
+
+	print "Looks like everything is ok and good to go"
+	
+
 
 
 
