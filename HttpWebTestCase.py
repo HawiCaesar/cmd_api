5d1ab5532407445ef98037fe7efd221d36e0870e
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-14 17:49:30 +0300

    Add test cases for 2 status codes, build more functionality for post function to api

--- HttpWebTestCase.py
+++ HttpWebTestCase.py
@@ -3,11 +3,11 @@ from http_web import get_api_data, post_data_to_api
 
 class HttpWebTestCase(unittest.TestCase):
 
-	api_url = 'https://jsonplaceholder.typicode.com/posts'
+	api_url = 'https://jsonplaceholder.typicode.com/posts/'
 
 	# Check for result from API
 	def test_api_data(self):
-		self.assertIsInstance(get_api_data(self.api_url, '3'), requests.models.Response, "200 OK from API can is reachable and accessible")
+		self.assertIsInstance(get_api_data(self.api_url, '3'), requests.models.Response, "Data from API can is reachable and accessible")
 
 	# Check if error is raised for non int values
 	def test_get_api_data_raises_error_non_int_values(self):
@@ -18,5 +18,15 @@ class HttpWebTestCase(unittest.TestCase):
 	def test_api_data_exceed_post_id(self):
 		self.assertIsInstance(get_api_data(self.api_url, '310'), requests.models.Response, "Return response even though no data is NOT found on API")
 
+	# Check for 200OK result
+	def test_api_data_correct_response200OK(self):
+		response = get_api_data(self.api_url, '4')
 
+		self.assertEqual(response.status_code, 200, "200 OK response for reachable API")
+
+	# Check for 404 not found result
+	def test_api_data_correct_response404(self):
+		response = get_api_data(self.api_url, '310')
+
+		self.assertEqual(response.status_code, 404, "404 response no post found")
 
--- http_web.py
+++ http_web.py
@@ -7,28 +7,37 @@ def get_api_data(url, post_id):
 
 		response = requests.get(url + post_id)
 
-	except ValueError as e:
-		raise(e)
-		response = "No post macthes your request.Please enter an integer."
+	except :
+		raise ValueError("Please enter whole numbers from 1 to 100")
 
 	return response
 
-def post_data_to_api(post_url):
+def post_data_to_api(post_url, title, body):
 	pass
 
 def main():
-	"""
-
+	
 	print "******** Command Line API Application ********"
 
 	post_number = raw_input("Please enter a number between 1 and 100 \n")
 
 	print "Fetching your the post you want....\n"
-	"""
-
+	
 	response = get_api_data('https://jsonplaceholder.typicode.com/posts/', '3')
 
+	print("\tGET Response data\n\t%s\n%s\n\tStatus code\n\t%s\n%s\n\tHeaders\n\t%s\n%s" % 
+		("-"*17,response.text, "-"*11, response.status_code,"-"*7, response.headers))
 	
+	print "Looking good so far...."
+	print "Now let's create a post"
+
+	title = raw_input("Enter the title of your post: ")
+	body = raw_input("Enter the body for your post: ")
+
+	print("\n\tCreating your new post...\n")
+	post_r = post_data_to_api('https://jsonplaceholder.typicode.com/posts/', title, body)
+	print("\tPOST Response data\n\t%s\n%s\n\tStatus code\n\t%s\n%s\n\tHeaders\n\t%s\n%s" % 
+		("-"*17,post_r.text, "-"*11, post_r.status_code,"-"*7, post_r.headers))
 
 
 
