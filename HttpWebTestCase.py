5575feeffce48242aa2ed0b4831b753b4ae1955c
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-14 18:12:41 +0300

    Add test case for created resource, test case to ensure result from API

--- HttpWebTestCase.py
+++ HttpWebTestCase.py
@@ -7,16 +7,16 @@ class HttpWebTestCase(unittest.TestCase):
 
 	# Check for result from API
 	def test_api_data(self):
-		self.assertIsInstance(get_api_data(self.api_url, '3'), requests.models.Response, "Data from API can is reachable and accessible")
+		self.assertIsInstance(get_api_data(self.api_url, '3'), requests.models.Response, "Data from API should reachable and accessible")
 
 	# Check if error is raised for non int values
 	def test_get_api_data_raises_error_non_int_values(self):
 		with self.assertRaises(ValueError):
 			get_api_data(self.api_url, 'abc')
 
-	# Check for no result, 404
+	# Check for no result but return response
 	def test_api_data_exceed_post_id(self):
-		self.assertIsInstance(get_api_data(self.api_url, '310'), requests.models.Response, "Return response even though no data is NOT found on API")
+		self.assertIsInstance(get_api_data(self.api_url, '310'), requests.models.Response, "Return response must be there even though no data is NOT found on API")
 
 	# Check for 200OK result
 	def test_api_data_correct_response200OK(self):
@@ -28,5 +28,15 @@ class HttpWebTestCase(unittest.TestCase):
 	def test_api_data_correct_response404(self):
 		response = get_api_data(self.api_url, '310')
 
-		self.assertEqual(response.status_code, 404, "404 response no post found")
+		self.assertEqual(response.status_code, 404, "404 response when no post_id found")
+
+	# Check for result from API when doing a POST request
+	def test_post_data_to_api(self):
+		self.assertIsInstance(post_data_to_api(self.api_url, 'Trial 1', 'This is a test'), requests.models.Response, "Data not posted to API")
+
+	# Check for 201 whether post has been created
+	def test_api_data_correct_response201(self):
+		response = post_data_to_api(self.api_url, 'Trial 1', 'This is a test')
+
+		self.assertEqual(response.status_code, 201, "201 response no post_id found")
 
--- http_web.py
+++ http_web.py
@@ -13,11 +13,18 @@ def get_api_data(url, post_id):
 	return response
 
 def post_data_to_api(post_url, title, body):
-	pass
+	post_data = {
+			'title': title,
+			'body': body,
+			'userId': 1
+		}
+	response = requests.post(post_url, data=post_data)
+
+	return response
 
 def main():
 	
-	print "******** Command Line API Application ********"
+	print "******** Command Line Http Web API App ********"
 
 	post_number = raw_input("Please enter a number between 1 and 100 \n")
 
@@ -38,6 +45,10 @@ def main():
 	post_r = post_data_to_api('https://jsonplaceholder.typicode.com/posts/', title, body)
 	print("\tPOST Response data\n\t%s\n%s\n\tStatus code\n\t%s\n%s\n\tHeaders\n\t%s\n%s" % 
 		("-"*17,post_r.text, "-"*11, post_r.status_code,"-"*7, post_r.headers))
+
+	print "\n\n\n"
+
+	print "Good Stuff! Thank you for consuming the API"
 
 
 
